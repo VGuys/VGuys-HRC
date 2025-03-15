8796883fd85887f1446be43a5ccd8a91b13468c3
Adam Burri <125667469+aburri19@users.noreply.github.com>
2025-03-15 15:14:44 +0400

    Changed the port

--- health_risk.js
+++ health_risk.js
@@ -56,7 +56,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1337;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
